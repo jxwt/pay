0f4157197b10e9d648ff3eabb7edb88c098a83bf
hcf <hcf@MACdeMacBook.local>
2020-06-19 15:26:01 +0800

    添加请求参数

--- param.go
+++ param.go
@@ -17,17 +17,22 @@ type CommonResponse struct {
 	Data    interface{}
 }
 
-// DoPayRequest 发起支付请求
+// DoPayRequest 支付请求
 type DoPayRequest struct {
+	ServiceID     int     `json:"serviceID"`     // 服务ID
 	Name          string  `json:"name"`          // 服务名称
 	TenantID      int     `json:"tenantID"`      // 商户ID
-	UserID        int     `json:"userID"`        // 系统内的用户ID(可空)
-	ThirdUserID   string  `json:"thirdUserID"`   // 三方用户ID(可空)
 	OrderID       int     `json:"orderID"`       // 订单号 或者 batchID
 	Money         float64 `json:"money"`         // 金额
 	CashReasonID  int     `json:"cashReasonID"`  // 支付原因ID
 	CashChannelID int     `json:"cashChannelID"` // 支付方式ID
 	CallBackURL   string  `json:"callBackURL"`   // 支付回调地址(内部回调地址)
+	// 非必须参数
+	UserID        int     `json:"userID"`        // 系统内的用户ID(可空)
+	ThirdUserID   string  `json:"thirdUserID"`   // 三方用户ID(可空)
+	TradeNumber   string  `json:"tradeNumber"`   // 商户单号(只用于存两份cashflow的服务,目前只有停车服务需要)
+	DiscountMoney float64 `json:"discountMoney"` // 优惠金额
+	PresentMoney  float64 `json:"presentMoney"`  // 赠送金额
 }
 
 // 服务注册参数
