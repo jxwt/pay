c4f00b5418a4953bf21e9a9f92a0f9302725b460
hcf <h516134941@163.com>
2021-04-09 14:51:18 +0800

    添加sub_appid兼容

--- wxpay/wxpay.go
+++ wxpay/wxpay.go
@@ -14,10 +14,11 @@ import (
 type WxClient struct {
 	AppID       string
 	MchID       string
-	SecretKey   string  // 登录秘钥
-	PayKey  	string 	// 支付加签秘钥
+	SecretKey   string // 登录秘钥
+	PayKey      string // 支付加签秘钥
 	CallbackURL string
 	SubMchId    string
+	SubAppId    string
 
 	CertPEM string // cert证书
 	KeyPEM  string // 密钥证书
@@ -29,19 +30,21 @@ type WxClient struct {
 	SerialNo   string // 敏感信息加密使用的证书号
 }
 
-func InitWxClient(AppID string, MchID string, SecretKey string, PayKey string, CallbackURL string, subMchId ...string) *WxClient {
+func InitWxClient(AppID string, MchID string, SecretKey string, PayKey string, CallbackURL string, subMchId string, subAppId string) *WxClient {
 	c := &WxClient{
 		AppID:       AppID,
 		MchID:       MchID,
 		SecretKey:   SecretKey,
-		PayKey:  	PayKey,
+		PayKey:      PayKey,
 		CallbackURL: CallbackURL,
 		httpsClient: nil,
 	}
-	if len(subMchId) > 0 {
-		c.SubMchId = subMchId[0]
+	if subMchId != "" {
+		c.SubMchId = subMchId
 	}
-
+	if subAppId != "" {
+		c.SubAppId = subAppId
+	}
 	return c
 }
 
@@ -135,6 +138,9 @@ func (i *WxClient) WxUnifiedOrder(charge *Charge, tradeType string) (WeChatQuery
 	if i.SubMchId != "" {
 		m["sub_mch_id"] = i.SubMchId
 	}
+	if i.SubAppId != "" {
+		m["sub_appid"] = i.SubAppId
+	}
 	if charge.OpenID != "" {
 		m["openid"] = charge.OpenID
 	}
@@ -154,6 +160,7 @@ func (i *WxClient) WxUnifiedOrder(charge *Charge, tradeType string) (WeChatQuery
 	m["sign"] = sign
 	*result, err = PostWechat("https://api.mch.weixin.qq.com/pay/unifiedorder", m, nil)
 	if err != nil {
+		logs.Warning(m)
 		return *result, err
 	}
 	return *result, err
